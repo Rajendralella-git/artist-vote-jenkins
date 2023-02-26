92d155f99980f586bb13a69e274fef5ffb467c67
Rajendralella-git <71184133+Rajendralella-git@users.noreply.github.com>
2023-02-26 19:18:11 +0530

    added post construct in main class

--- src/main/java/com/artist/vote/demo/ArtistVoteApplication.java
+++ src/main/java/com/artist/vote/demo/ArtistVoteApplication.java
@@ -1,5 +1,7 @@
 package com.artist.vote.demo;
 
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArtistVoteApplication {
 	private static Logger logger = LoggerFactory.getLogger(ArtistVoteApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("application started");
+	}
 
 	public static void main(String[] args) {
 		logger.info("Test");
