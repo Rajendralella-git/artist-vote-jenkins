4b186120a05c8139796cfaa46ea93ce7afd023b6
Rajendralella-git <71184133+Rajendralella-git@users.noreply.github.com>
2023-02-26 18:54:39 +0530

    added loggers

--- src/main/java/com/artist/vote/demo/ArtistVoteApplication.java
+++ src/main/java/com/artist/vote/demo/ArtistVoteApplication.java
@@ -1,12 +1,16 @@
 package com.artist.vote.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ArtistVoteApplication {
+	private static Logger logger = LoggerFactory.getLogger(ArtistVoteApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("Test");
 		SpringApplication.run(ArtistVoteApplication.class, args);
 	}
 }
